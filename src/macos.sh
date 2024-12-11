184831fecee1bfff67c53195e7d26af492bc6bff
Bas Maas <bas.maas@ing.com>
2024-12-11 15:03:05 +0100

    Test

--- src/macos.sh
+++ src/macos.sh
@@ -4,7 +4,7 @@
 which -s brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Install Ansible
-which -s ansible || brew install ansible
+which -s ansible || brew install ansible        # TODO: Add fixed version
 
 # Install Xcode Command Line Tools
 xcode-select -p 1>/dev/null 2>/dev/null || xcode-select --install
