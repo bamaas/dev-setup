5391a74df3b555a46a34ff384f9cb08a7329500e
Bas Maas <bas.maas@ing.com>
2024-12-11 14:59:14 +0100

    Brew install ansible

--- src/macos.sh
+++ src/macos.sh
@@ -4,7 +4,7 @@
 which -s brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Install Ansible
-which -s ansible || brew install ansible@10
+which -s ansible || brew install ansible
 
 # Install Xcode Command Line Tools
 xcode-select -p 1>/dev/null 2>/dev/null || xcode-select --install
