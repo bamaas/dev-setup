56aa918031cb1b6588537cbe0abc8ea818bf871b
Bas Maas <bas.maas@ing.com>
2024-12-13 10:14:56 +0100

    Added brew casks for macos

---
+++ src/macos-brew-casks.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+brew install --cask firefox
+brew install --cask standard-notes
+brew install --cask google-chrome
+brew install --cask visual-studio-code
+brew install --cask warp
+brew install --cask spotify
